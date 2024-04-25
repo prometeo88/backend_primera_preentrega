61e2c56ae4ca9b3418513d70910d0863dec72f79
Federico <f.ullua@gmail.com>
2024-04-25 19:48:14 -0300

    cart.id funcionado

--- src/routes/carts.js
+++ src/routes/carts.js
@@ -11,5 +11,28 @@ router.post('/carts', (req, res) =>{
     res.json(cartManager.carts)
 })
 
+router.post('/carts/:cid/product/:pid', (req, res) =>{
+    const cartData = req.body
+    cartManager.createCart(cartData)
+    
+    res.json(cartManager.carts)
+})
+
+router.get('/carts/:cid', (req, res) =>{
+    try {
+        const cartId = req.params.cid;
+        const cart = cartManager.carts.find( cart => cart.id === cartId)
+
+        if(!cart){
+            return res.status(400).json({message:'Carrito no encontrado'})
+        }
+
+        res.json(cart)
+    } catch (error) {
+        res.status(500).json({message:'Error en servidor' })
+        
+    }
+
+})
 
 module.exports = router
