c3819799042be12181d2d371f069b96f14aa9339
Federico <f.ullua@gmail.com>
2024-04-30 21:04:34 -0300

    implementacion handlersbars

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const expressHandlebars = require('express-handlebars');
 const app = express();
 const PORT = 8080;
+const http = require("http")
+const socketIo = require('socket.io')
+
+const server = http.createServer(app)
+const io = socketIo(server)
+
+io.on("connection", (socket) =>{
+    console.log("Nuevo cliente conectado con exito")
+})
+
+app.engine('handlebars', expressHandlebars.engine());
+app.set('views', __dirname + '/views')
+app.set('view engine','handlebars')
+app.use(express.static(__dirname + '/public'))
+
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -10,8 +27,11 @@ const cartsRouter = require("./routes/carts.js")
 app.use("/api/products",productsRouter)
 app.use("/api/carts",cartsRouter)
 
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
 
-app.listen(PORT, () =>{
+server.listen(PORT, () =>{
     console.log(`Server running on PORT ${PORT}`)
 })
